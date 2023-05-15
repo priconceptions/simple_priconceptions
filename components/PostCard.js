1defd3c56bc5b762ace6bdccad337d9ea9eddcb1
Priyanka Dhulkhed <priyanka@wave.com>
2023-05-15 12:35:08 -0400

    [2023-05-15] change styling of the blog again]

--- components/PostCard.js
+++ components/PostCard.js
@@ -4,16 +4,17 @@ import Link from 'next/link'
 const PostCard = ({ title, date, description, tags, subdir, slug }) => {
     return (
         <>
-        {title && title.length > 0 &&
-            <div className='post-card' style={{ marginBottom: "2rem" }}>
-                <Link href={`/${subdir}/${slug}`}>
-                    <a>{title}</a>
-                </Link>
-                <br></br>
-                {tags && tags.map((tag, index) => <small key={index} style={{ marginRight: "4px", fontStyle: "italic" }}>#{tag}</small>)}
-                <p>{description}</p>
-            </div>
-        }
+            {title && title.length > 0 &&
+                <div className='post-card'>
+                    <Link href={`/${subdir}/${slug}`}>
+                        <a>{title}</a>
+                    </Link>
+                    <span>
+                        {tags && tags.map((tag, index) => <small key={index} style={{ marginRight: "4px", fontStyle: "italic" }}>#{tag}</small>)}
+                    </span>
+                    <p>{description}</p>
+                </div>
+            }
         </>
     )
 }

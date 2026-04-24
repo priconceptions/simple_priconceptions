e74235be72224c30b59409cd3afeaff981ae2c4d
Priyanka Dhulkhed <priyanka@wave.com>
2026-04-24 17:24:53 -0400

    fix font

--- components/Meta.js
+++ components/Meta.js
@@ -31,7 +31,9 @@ export default function Meta() {
             <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
             <meta name="theme-color" content="#000" />
             <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-            <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap" rel="stylesheet" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+            <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;600;700&display=swap" rel="stylesheet" />
             <meta
                 name="description"
                 content={`Priyanka's internet home`}
